79f6cf992d9c08cf9bf31da0b2c6268a65f892a6
Chris Violette <cvioletterun@gmail.com>
2013-04-16 17:09:29 -0400

    Updating archive templates to show excerpts

--- wp-content/themes/pchc/index.php
+++ wp-content/themes/pchc/index.php
@@ -8,7 +8,7 @@
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-								<?php get_template_part( 'content', get_post_format() ); ?>
+								<?php get_template_part( 'content-excerpt', 'index' ); ?>
 
 							<?php endwhile; ?>
 
