ed841371c2ee11cab2524b67ef334c2ac03d00e0
Chris Violette <cvioletterun@gmail.com>
2013-04-29 13:55:48 -0400

    Updating SQL script

--- mysql_update.sql
+++ mysql_update.sql
@@ -1,1 +1,3 @@
-UPDATE pchc_options SET option_value = replace(option_value, 'http://localhost:8888/pchc', 'http://web.pchc.local') WHERE option_value LIKE '%http://localhost:8888/pchc%';
+UPDATE pchc_options SET option_value = replace(option_value, 'http://localhost:8888/web.pchc.local', 'http://web.pchc.local') WHERE option_value LIKE '%http://localhost:8888/web.pchc.local%';
+UPDATE pchc_postmeta SET meta_value = replace(meta_value, 'http://localhost:8888/web.pchc.local', 'http://web.pchc.local') WHERE meta_value LIKE '%http://localhost:8888/web.pchc.local%';
+UPDATE pchc_posts SET guid = replace(guid, 'http://localhost:8888/web.pchc.local', 'http://web.pchc.local') WHERE guid LIKE '%http://localhost:8888/web.pchc.local%';
