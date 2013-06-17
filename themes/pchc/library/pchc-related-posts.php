11552032da9e4719d1b637821ae1a7b4ed0f969f
Chris Violette <cvioletterun@gmail.com>
2013-06-17 16:57:48 -0400

    Updated related posts widget

--- themes/pchc/library/pchc-related-posts.php
+++ themes/pchc/library/pchc-related-posts.php
@@ -28,6 +28,8 @@ class PCHC_Related_Posts extends WP_Widget {
 			'id_base' => 'pchc-related-posts' 
 		);
 		
+		$GLOBALS['show_services_list'] = true;
+		
 		$this->WP_Widget( 'pchc-related-posts', __('PCHC Related Posts', 'pchcrelatedposts'), $widget_ops, $control_ops );
 	}
 	
@@ -39,6 +41,8 @@ class PCHC_Related_Posts extends WP_Widget {
 		$post_ids = get_post_meta( $post->ID, $instance['post_type'], false );
 
         if( !empty( $post_ids ) && is_single() ) {
+        
+        	$GLOBALS['show_services_list'] = false;
 
 			//Our variables from the widget settings.
 			$title = apply_filters('widget_title', $instance['title'] );
@@ -77,6 +81,48 @@ class PCHC_Related_Posts extends WP_Widget {
             
             echo $after_widget;
 	        
+	    } else {
+		    if( $GLOBALS['show_services_list'] ) {
+		    
+		    	$GLOBALS['show_services_list'] = false;
+
+				//Our variables from the widget settings.
+				$title = apply_filters('widget_title', 'Services' );
+				
+				echo $before_widget;
+		
+				// Display the widget title 
+				if ( $title )
+					echo $before_title . $title . $after_title;
+	
+			
+	
+		        $args = array(
+		            'post_type'         =>  'service',
+		            'post_status'       =>  'publish',
+		            'posts_per_page'    =>  -1,
+		            'orderby'           =>  'title',
+		        );
+		
+		        $the_posts = new WP_Query( $args );
+		        
+		        //print_r($the_posts);
+		
+		        if( $the_posts->have_posts() ) { 
+		        
+			        echo '<ul>';
+			      
+			        while( $the_posts->have_posts() ) { $the_posts->the_post(); ?>
+	                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+	                <?php }
+	               
+	                echo '</ul>';
+	             
+	            }
+	            
+	            echo $after_widget;
+            
+		    }
 	    }
 	    
 	    wp_reset_postdata();
