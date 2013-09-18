3a0a8d05fb3b814db9932f1fe31e9bc9b9732fa6
Chris Violette <cvioletterun@gmail.com>
2013-09-18 11:51:33 -0400

    Updated output of search results.

--- themes/pchc/search.php
+++ themes/pchc/search.php
@@ -10,7 +10,7 @@
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						
-							<?php get_template_part( 'content-excerpt', get_post_format() ); ?>
+							<?php get_template_part( 'search-excerpt', get_post_format() ); ?>
 
 						<?php endwhile; ?>
 
