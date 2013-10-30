a498515221a8e59a2fe24ff69c4e94eb5891a972
PCHC <info@pchc.com>
2013-10-30 15:03:34 -0400

    Added Pingdom script to header

--- wp-content/themes/pchc/header.php
+++ wp-content/themes/pchc/header.php
@@ -53,6 +53,19 @@
 		</script>
 		<!-- end analytics -->
 		
+		<!-- Pingdom monitoring -->
+		<script>
+		var _prum = [['id', '52713701abe53d4463000000'],
+		             ['mark', 'firstbyte', (new Date()).getTime()]];
+		(function() {
+		    var s = document.getElementsByTagName('script')[0]
+		      , p = document.createElement('script');
+		    p.async = 'async';
+		    p.src = '//rum-static.pingdom.net/prum.min.js';
+		    s.parentNode.insertBefore(p, s);
+		})();
+		</script>
+		
 		<!-- W3TC-include-js-head -->
 
 	</head>
