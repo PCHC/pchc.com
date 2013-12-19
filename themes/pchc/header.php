4b5eb2bc98d4b280b110f8b8078e92c21b692a99
Chris Violette <cvioletterun@gmail.com>
2013-12-19 10:23:32 -0500

    Added analytics field to theme customization options removed hard-coded analytics from header.php.

--- themes/pchc/header.php
+++ themes/pchc/header.php
@@ -36,22 +36,6 @@
 		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
 		<!-- end of wordpress head -->
-
-		<!-- drop Google Analytics Here -->
-		<script type="text/javascript">
-
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-22274651-1']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		
-		</script>
-		<!-- end analytics -->
 		
 		<!-- Pingdom monitoring -->
 		<script>
