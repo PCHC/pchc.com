0102f623d8940fbb6b85a86435b5bb1950367240
Chris Violette <cvioletterun@gmail.com>
2014-03-11 10:52:21 -0400

    Fixed staff archive to display properly.

--- themes/pchc-pediatrics/content-excerpt-staff.php
+++ themes/pchc-pediatrics/content-excerpt-staff.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix content-excerpt content-excerpt-provider'); ?> role="article">
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix content-excerpt content-excerpt-provider type-provider'); ?> role="article">
 
 	<?php $the_meta = get_post_meta( $post->ID ); ?>
 
