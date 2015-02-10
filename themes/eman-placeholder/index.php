adf1ea38ab9da3120704b5bbe1811761af3fe7d5
Chris Violette <cvioletterun@gmail.com>
2015-02-10 15:22:44 -0500

    Code cleanup

--- themes/eman-placeholder/index.php
+++ themes/eman-placeholder/index.php
@@ -5,14 +5,14 @@
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-  <head>
-    <meta charset="utf-8">
+<head>
+  <meta charset="utf-8">
 
-    <!-- Google Chrome Frame for IE -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta http-equiv="refresh" content="10;URL=http://www.mainehealthequity.org">
-    
-    <!-- W3TC-include-css -->
+  <!-- Google Chrome Frame for IE -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="refresh" content="10;URL=http://www.mainehealthequity.org">
+
+  <!-- W3TC-include-css -->
 
   <title>Eastern Maine AIDS Network</title>
 
@@ -29,21 +29,21 @@
 <body <?php body_class(); ?>>
 
   <div class="wrap"><div class="container">
-  <header>
-  	<a href="http://www.mainehealthequity.org">
-	  	<img src="<?php echo get_template_directory_uri(); ?>/img/eman-logo.png" alt="Eastern Maine AIDS Network">
-	    <h1><strong>EMAN</strong> is now part of <strong>Down&nbsp;East AIDS&nbsp;Network.</strong></h1>
-	    <h2>Click here to be redirected to <strong>mainehealthequity.org</strong>.</h2>
-    </a>
-  </header>
-  <footer>
-  	<a href="http://pchc.com">
-    <img src="<?php echo get_template_directory_uri(); ?>/img/pchc-logo.png" alt="Penobscot Community Health Care">
-    </a>
-  </footer>
-</div></div>
+    <header>
+      <a href="http://www.mainehealthequity.org">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/eman-logo.png" alt="Eastern Maine AIDS Network">
+        <h1><strong>EMAN</strong> is now part of <strong>Down&nbsp;East AIDS&nbsp;Network.</strong></h1>
+        <h2>Click here to be redirected to <strong>mainehealthequity.org</strong>.</h2>
+      </a>
+    </header>
+    <footer>
+      <a href="http://pchc.com">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/pchc-logo.png" alt="Penobscot Community Health Care">
+      </a>
+    </footer>
+  </div></div>
 
-<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 </body>
 
 </html>
